040ca900e37cd4f10c2fe3eed99ff74bf6303dea
yuig100 <54219998+yuig100@users.noreply.github.com>
2022-09-01 00:17:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,114 @@
 //Recebendo a questão e as alternativas
-let pergunta = {
+let perguntas = [
+    
+{
     titulo: 'Gato',
     alternativas: ['gat','cat','gate','dog'],
     correta: 1
-};
+},
+{
+    titulo: 'Cachorro',
+    alternativas: ['dinosaur','dimension','dinner','dog'],
+    correta: 3
+},
+{
+    titulo: 'Fire',
+    alternativas: ['fogo','fotball','fiction','fax'],
+    correta: 0
+},
+{
+    titulo: 'Bird',
+    alternativas: ['pinoquio','panela','passaro','pistache'],
+    correta: 2
+}
 
-//função de mostrar a questão na tela
-function mostraquestao(q){
-    let titleDiv = document.getElementById('titulo');
-    titleDiv.textContent = q.titulo;
+]
+
+
+//Metodo do Quiz
+let app = {
+    
+//Função inicio
+start: function(){
+    
+    this.Atualpos = 0;
+    this.Totalpontos = 0;
     
     let alts= document.querySelectorAll('.alternativa');
-    alts.forEach(function(element,index){
-        element.textContent = q.alternativas[index];
-        element.addEventListener('click',function(){
+    alts.forEach((element,index)=>{
+        element.addEventListener('click',()=>{
             
-            if(index == q.correta){
-                
-                console.log("Acertou!");
-                
-            } else {
-                
-                console.log("Errrrroooooou!");
-                
-            }
+            this.checaResposta(index);
             
         })
     })
     
+    this.atualizaPontos();
+    app.mostraquestao(perguntas[this.Atualpos]);
+    
+},
+
+//função de mostrar a questão na tela
+mostraquestao: function(q){
+    
+    //Pegando a questão atual
+    this.qatual = q;
+    
+    //mostrando o titulo
+    let titleDiv = document.getElementById('titulo');
+    titleDiv.textContent = q.titulo;
+    
+    //mostrando as alternativas
+    let alts= document.querySelectorAll('.alternativa');
+    alts.forEach(function(element,index){
+        element.textContent = q.alternativas[index];
+
+    })
+    
+},
+
+//Avançar para a proxima pergunta    
+Proximaperg: function(){
+    
+    this.Atualpos++;
+    
+    if(this.Atualpos == perguntas.length){
+    
+        this.Atualpos = 0;
+    
+    }
+    
+},    
+
+//Chegar que a resposta está certa
+checaResposta: function(user){
+    
+    if(this.qatual.correta == user){
+        
+        console.log("Certa");
+        this.Totalpontos++;
+        
+    } else {
+        
+        console.log("Errada");
+        
+    }
+    
+    this.atualizaPontos();
+    this.Proximaperg();
+    this.mostraquestao(perguntas[this.Atualpos]);
+},
+
+//Atualizar a pontuação do usuario    
+atualizaPontos: function(){
+    
+    let scoreDiv = document.getElementById('pontos');
+    scoreDiv.textContent = `Points: ${this.Totalpontos}`; 
+    
+}    
+    
 }
 
-//parte da main
-mostraquestao(pergunta);
+
+//main
+app.start();
